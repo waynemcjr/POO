cd8937aea76674c52796b4a2da83104108ea2cf5
waynemcjr <waynebarbosa10@hotmail.com>
2025-10-15 09:53:23 -0300

    Iniciação de POO em C#

--- POOExercicio2/Program.cs
+++ POOExercicio2/Program.cs
@@ -9,42 +9,45 @@ Funcionario[] funcionarios = new Funcionario[qtdFuncionarios];
 
 for (int i = 0; i < funcionarios.Length; i++)
 {
-    Console.Write("\nInforme o tipo de funcionario (1 - CLT ou 2 PJ): ");
-    tipo = Convert.ToInt32(Console.ReadLine()!);
-
-    if (tipo == 1)
+    do
     {
-        funcionarios[i] = new FuncionarioCLT();
+        Console.Write("\nInforme o tipo de funcionario (1 - CLT ou 2 PJ): ");
+        tipo = Convert.ToInt32(Console.ReadLine()!);
 
-        Console.Write("Informe o nome: ");
-        funcionarios[i].setNome(Console.ReadLine()!);
+        if (tipo == 1)
+        {
+            funcionarios[i] = new FuncionarioCLT();
 
-        Console.Write("Informe o salário base: ");
-        funcionarios[i].setSalarioBase(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Informe o nome: ");
+            funcionarios[i].setNome(Console.ReadLine()!);
 
-        Console.Write("Informe o bônus: ");
-        ((FuncionarioCLT)funcionarios[i]).setBonus(Convert.ToDouble(Console.ReadLine()));
-    }
-    else if (tipo == 2)
-    {
-        funcionarios[i] = new FuncionarioPJ();
+            Console.Write("Informe o salário base: ");
+            funcionarios[i].setSalarioBase(Convert.ToDouble(Console.ReadLine()));
 
-        Console.Write("Informe o nome: ");
-        funcionarios[i].setNome(Console.ReadLine()!);
+            Console.Write("Informe o bônus: ");
+            ((FuncionarioCLT)funcionarios[i]).setBonus(Convert.ToDouble(Console.ReadLine()));
+        }
+        else if (tipo == 2)
+        {
+            funcionarios[i] = new FuncionarioPJ();
 
-        Console.Write("Informe o salário base: ");
-        ((FuncionarioPJ)funcionarios[i]).setSalarioBase(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Informe o nome: ");
+            funcionarios[i].setNome(Console.ReadLine()!);
 
-        Console.Write("Informe as horas trabalhadas: ");
-        ((FuncionarioPJ)funcionarios[i]).setHorasTrabalhadas(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Informe o salário base: ");
+            ((FuncionarioPJ)funcionarios[i]).setSalarioBase(Convert.ToDouble(Console.ReadLine()));
 
-        Console.Write("Informe o valor da hora: ");
-        ((FuncionarioPJ)funcionarios[i]).setValorHora(Convert.ToDouble(Console.ReadLine()));
-    }
-    else
-    {
-        Console.WriteLine("Informe um número válido (1 ou 2)!");
-    }
+            Console.Write("Informe as horas trabalhadas: ");
+            ((FuncionarioPJ)funcionarios[i]).setHorasTrabalhadas(Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Informe o valor da hora: ");
+            ((FuncionarioPJ)funcionarios[i]).setValorHora(Convert.ToDouble(Console.ReadLine()));
+        }
+        else
+        {
+            Console.WriteLine("Informe um número válido (1 ou 2)!");
+        }
+    } while (tipo != 1 && tipo != 2);
 }
 
 Console.WriteLine();
